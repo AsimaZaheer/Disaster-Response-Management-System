3f04c35617587432204a89db2c697806df29305f
AsimaZaheer <asimazaheer2006@gmail.com>
2025-07-02 23:17:04 +0500

    Update AdvancedMedicalSupplies.java

--- AdvancedMedicalSupplies.java
+++ AdvancedMedicalSupplies.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package com.java.disasterresponsesystem;
-
+import java.util.Scanner;
 /**
  *
  * @author Dell
